e162fd9cc4460e8a4622443601fb4d3f2c67d018
Yeh9786 <yehyob9786@gmail.com>
2023-05-22 17:10:14 +0800

    new

---
+++ main.js
@@ -0,0 +1,7 @@
+pannellum.viewer('panorama-360-view', {
+    "type": "equirectangular",
+    "panorama": "https://i.imgur.com/kRMWpQ7.jpeg",
+    "autoLoad": true,
+    "title": "線上博物館",
+    "author": "台中文學館"
+})
---
+++ readme.md
@@ -0,0 +1,1 @@
+Web Museum Test
