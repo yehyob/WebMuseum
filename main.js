21f3897d5cbf2e7552c4d468580b18a2dbc09214
Yeh9786 <yehyob9786@gmail.com>
2023-05-23 16:29:25 +0800

    museum test

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ pannellum.viewer('panorama-360-view', {
     "title": "線上博物館",
     "author": "台中文學館"
 })
+
+// https://i.imgur.com/kRMWpQ7.jpeg scene 01
+// https://i.imgur.com/s6W6k3Q.jpeg scene 02
+// https://i.imgur.com/q5pmeyO.jpeg scene 03
