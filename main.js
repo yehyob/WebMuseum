ce859d3f0f51427577fe4250b55526f88417e1a8
Yeh9786 <yehyob9786@gmail.com>
2023-06-19 16:54:30 +0800

    completed museum

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ pannellum.viewer('panorama-360-view', {
     "author": "台中文學館"
 })
 
-// https://i.imgur.com/kRMWpQ7.jpeg scene 01
-// https://i.imgur.com/s6W6k3Q.jpeg scene 02
-// https://i.imgur.com/q5pmeyO.jpeg scene 03
+// https://i.imgur.com/98QYKMn.jpg 001 entrance
+// https://i.imgur.com/ZR3slTj.jpg 002 near entrance
+// https://i.imgur.com/MohXmfo.jpg 003 center in main room
+// https://i.imgur.com/h2N1nJC.jpg 004 author avatar left
+// https://i.imgur.com/fXDCOMH.jpg 005 author avatar right
+// https://i.imgur.com/5IpELCV.jpg 006 aisle 01
+// https://i.imgur.com/DxbX10b.jpg 007 aisle 02
+// https://i.imgur.com/cSMMtGj.jpg 008 aisle 03
+// https://i.imgur.com/zgrnDhe.jpg 009 aisle 04
+// https://i.imgur.com/sLS2Ngf.jpg 010 aisle 05
+// https://i.imgur.com/AFNF7IF.jpg 011 center in side room
+// https://i.imgur.com/AJD9YSq.jpg 012 corner in side room
+// https://i.imgur.com/2l1AaUQ.jpg 013 side room left
+// https://i.imgur.com/Qhih00y.jpg 014 side room right
+// https://i.imgur.com/Fm4Ng3h.jpg 015 author note 01
+// https://i.imgur.com/Ev8Xnal.jpg 016 author note 02
+// https://i.imgur.com/gnHWmPp.jpg 017 author note 03
+// https://i.imgur.com/AsC0eb9.jpg 018 author note 04
+// https://i.imgur.com/9dFWAJz.jpg 019 author note 05
+// https://i.imgur.com/xx98v6q.jpg 020 author note 06
